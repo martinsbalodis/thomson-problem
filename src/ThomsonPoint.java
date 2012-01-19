1d6f1872acad4215b4aceb183d73be48f614a079
Martins Balodis <martins256@gmail.com>
2012-01-19 22:16:54 +0200

    Added: point initialization, vector normalization method

--- src/ThomsonPoint.java
+++ src/ThomsonPoint.java
@@ -11,33 +11,55 @@ public class ThomsonPoint {
 	
 	double[] point;
 	
+	public void ThomsonPoint() {
+		
+		// initialize  memory for point
+		this.point = new double[3];
+		
+	}
+	
 	/**
 	 * Puts point in a random place on sphere
 	 * @param sphere 
 	 */
 	public void put_in_random_position(ThomsonSphere sphere) {
 		
-		double[] point1 = {0,0,0};
-		double[] point2 = {0,0,1};
-		
-		this.point = Geometry.createVector(point1, point2);
-		
-		
-		
+		// create point as a normalized vector
+		this.point[0] = Math.random()-.5;
+		this.point[1] = Math.random()-.5;
+		this.point[2] = Math.random()-.5;
+		this.point = normalizeVector(this.point);
+				
 		// @TODO check point colission
 		
 	}
 	
 	/**
 	 * Calculate energy between two points
-	 * @param vector
-	 * @return 
+	 * @param point
+	 * @return double
 	 */
-	public double get_energy(double[] vector) {
+	public double get_energy(double[] point) {
 		
-		double distance = Geometry.length(this.point, vector);
+		double distance = Geometry.length(this.point, point);
 		
 		return 1/distance;
+	}
+	
+	/**
+	 * Normalize vector
+	 * @param vector
+	 * @return vector
+	 */
+	public static double[] normalizeVector(double[] vector) {
+		
+		double length = Geometry.length(vector);
+		
+		vector[0] /=length;
+		vector[1] /=length;
+		vector[2] /=length;
+		
+		return vector;
 	}
 	
 }
