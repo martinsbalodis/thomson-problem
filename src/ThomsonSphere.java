0885be29d237f50ce55055870b58ff7057055e5b
Martins Balodis <martins256@gmail.com>
2012-01-20 17:52:58 +0200

    Added: find maximum energy neighbours

--- src/ThomsonSphere.java
+++ src/ThomsonSphere.java
@@ -30,19 +30,60 @@ public class ThomsonSphere {
 	}
 	
 	/**
+	 * Update point energy
+	 */
+	private void update_point_energy() {
+		
+		for(ThomsonPoint p : this.points) {
+			p.calculate_energy(this.points);
+		}
+		
+	}
+	
+	/**
+	 * Returns point with maximum energy
+	 * @return ThomsonPoint
+	 */
+	public ThomsonPoint get_maximum_energy_point() {
+
+		ThomsonPoint maximum_energy_point = points[0];
+
+		for (ThomsonPoint p : this.points) {
+			if (p.energy > maximum_energy_point.energy) {
+				maximum_energy_point = p;
+			}
+		}
+
+		return maximum_energy_point;
+	}
+	
+	/**
 	 * Arrange to their correct places
 	 */
 	public void arrange_points() {
 		
-		ThomsonPoint p1;
-		ThomsonPoint p2;
+		// update point energy
+		this.update_point_energy();
 		
 		// Find point with maximum energy
+		ThomsonPoint maximum_energy_point = this.get_maximum_energy_point();
+		
+		// Find closest, second closest neighbour
+		double energy_between, maximum_energy_between = 0;
+		ThomsonPoint maximum_energy_neighbour, maximum_energy_neighbour2;
+		
 		for(ThomsonPoint p : this.points) {
-			
+			if(p!=maximum_energy_point) {
+				
+				energy_between = maximum_energy_point.get_energy(p);
+				if(energy_between > maximum_energy_between) {
+
+					maximum_energy_neighbour2 = maximum_energy_neighbour;
+					maximum_energy_neighbour = p;
+					maximum_energy_between = energy_between;
+				}
+			}
 		}
-		
-		
 		
 	}
 }
