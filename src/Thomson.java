ec8d6ad8796ac354ed4d8331aa143c04c5ec5874
Mārtiņš Balodis <martins256@gmail.com>
2012-01-21 17:03:04 +0200

    Load configuration as args

--- src/Thomson.java
+++ src/Thomson.java
@@ -13,34 +13,43 @@ public class Thomson {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		
-		ThomsonSphere sphere = new ThomsonSphere(100);
-		
-		ThomsonPreview preview = new ThomsonPreview(sphere);
-		
+
+		int point_count = Integer.parseInt(args[0]);
+		boolean display_sphere = Boolean.parseBoolean(args[1]);
+
+		ThomsonSphere sphere = new ThomsonSphere(point_count);
+
 		double smallest_found_energy = -1;
 		
-		for(int i = 0;i<=100000;i++) {
-			
-			// Output current sphere energy
-			double sphere_energy = sphere.get_energy();
-			if(smallest_found_energy > sphere_energy || smallest_found_energy == -1) {
-				smallest_found_energy = sphere_energy;
-			}
-			
-			sphere.arrange_points();
-			preview.repaint();
-			
-//			try {
-//				//do what you want to do before sleeping
-//				Thread.currentThread().sleep(100);//sleep for 1000 ms
-//				//do what you want to do after sleeptig
-//			} catch (Exception e) {
-//				//If this thread was intrrupted by nother thread 
-//			}
+		// Run width formpanel
+		if (display_sphere) {
 
+			ThomsonPreview preview = new ThomsonPreview(sphere);
+
+			for (int i = 0; i <= 100000; i++) {
+
+				double sphere_energy = sphere.get_energy();
+				if (smallest_found_energy > sphere_energy || smallest_found_energy == -1) {
+					smallest_found_energy = sphere_energy;
+				}
+
+				sphere.arrange_points();
+				preview.repaint();
+			}
+		} 
+		else {
+			for (int i = 0; i <= 100000; i++) {
+
+				double sphere_energy = sphere.get_energy();
+				if (smallest_found_energy > sphere_energy || smallest_found_energy == -1) {
+					smallest_found_energy = sphere_energy;
+				}
+				
+				sphere.arrange_points();
+			}
 		}
-		
+
+		// Print results
 		System.out.println("Smallest found energy " + smallest_found_energy);
 	}
 }
