369b1a1fc9f4af97624990066dc87847844fa1d6
Martins Balodis <martins256@gmail.com>
2012-01-21 12:54:26 +0200

    Added: preview class for sphere

---
+++ src/ThomsonPreview.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author martins
+ */
+public class ThomsonPreview extends Canvas {
+	
+	ThomsonSphere sphere;
+	
+	public ThomsonPreview(ThomsonSphere sphere) {
+		
+		this.sphere = sphere;
+	}
+	
+	/**
+	 * Zīmē attēlu
+	 * @param graphics 
+	 */
+	@Override
+	public void paint(Graphics graphics) {
+		
+	}
+	
+}
